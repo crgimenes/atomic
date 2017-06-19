fd03db57e3dc9e9a2dc8f62d383e65db7db509cf
Cesar Gimenes <crg@crg.eti.br>
2017-06-18 22:13:14 -0300

    add simple tcp connection

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
-import "github.com/crgimenes/goConfig"
-import log "github.com/nuveo/logSys"
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	"github.com/crgimenes/goConfig"
+	log "github.com/nuveo/logSys"
+)
 
 type config struct {
 	Debug bool
+}
+
+func handleRequest(conn io.ReadWriteCloser) {
+
+	for {
+		buf := make([]byte, 1024)
+
+		reqLen, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+		}
+		fmt.Println("reqLen:", reqLen)
+		fmt.Println("buf:", string(buf))
+
+		conn.Write([]byte("Message received.\n"))
+
+		if buf[0] == 'q' {
+			conn.Close()
+		}
+	}
 }
 
 func main() {
@@ -20,4 +47,24 @@ func main() {
 		log.DebugMode = cfg.Debug
 		log.Warningln("debug mode on")
 	}
+
+	l, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+
 }
