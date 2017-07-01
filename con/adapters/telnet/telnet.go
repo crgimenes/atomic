1bf12d20d05eafed5aba6c52453c5dc1769e33f2
Cesar Gimenes <crg@crg.eti.br>
2017-07-01 18:52:19 -0300

    move telnet to package

---
+++ con/adapters/telnet/telnet.go
@@ -0,0 +1,63 @@
+package telnet
+
+import (
+	"fmt"
+	"net"
+)
+
+func Log(c byte) (r bool) {
+	switch c {
+	case 0x00:
+		fmt.Println("BINARY")
+	case 0x01:
+		fmt.Println("ECHO")
+	case 0x03:
+		fmt.Println("SGA")
+	case 0x18:
+		fmt.Println("TTYPE")
+	case 0x1F:
+		fmt.Println("NAWS")
+	case 0x22:
+		fmt.Println("LINEMODE")
+	case 0xF0:
+		fmt.Println("SE")
+	case 0xF1:
+		fmt.Println("NOP")
+	case 0xF2:
+		fmt.Println("DM")
+	case 0xf3:
+		fmt.Println("BRK")
+	case 0xF4:
+		fmt.Println("IP")
+	case 0xF5:
+		fmt.Println("AO")
+	case 0xF6:
+		fmt.Println("AYT")
+	case 0xF7:
+		fmt.Println("EC")
+	case 0xF8:
+		fmt.Println("EL")
+	case 0xF9:
+		fmt.Println("GA")
+	case 0xFA:
+		fmt.Println("SB")
+	case 0xFB:
+		fmt.Println("WILL")
+	case 0xFC:
+		fmt.Println("WONT")
+	case 0xFD:
+		fmt.Println("DO")
+	case 0xFE:
+		fmt.Println("DONT")
+	case 0xFF:
+		fmt.Println("IAC")
+	default:
+		r = true
+	}
+	return
+}
+
+func SendSetup(conn *net.TCPConn) (err error) {
+	_, err = conn.Write([]byte{255, 251, 3, 255, 251, 1})
+	return
+}
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"net"
 	"os"
 	"time"
 
+	"github.com/crgimenes/atomic/con/adapters/telnet"
 	"github.com/crgimenes/goConfig"
 	log "github.com/nuveo/logSys"
 )
@@ -16,98 +16,11 @@ type config struct {
 	Debug bool
 }
 
-func telnet(c byte) (r bool) {
-	switch c {
-	case 0x00:
-		fmt.Println("BINARY")
-	case 0x01:
-		fmt.Println("ECHO")
-	case 0x03:
-		fmt.Println("SGA")
-	case 0x18:
-		fmt.Println("TTYPE")
-	case 0x1F:
-		fmt.Println("NAWS")
-	case 0x22:
-		fmt.Println("LINEMODE")
-	case 0xF0:
-		fmt.Println("SE")
-	case 0xF1:
-		fmt.Println("NOP")
-	case 0xF2:
-		fmt.Println("DM")
-	case 0xf3:
-		fmt.Println("BRK")
-	case 0xF4:
-		fmt.Println("IP")
-	case 0xF5:
-		fmt.Println("AO")
-	case 0xF6:
-		fmt.Println("AYT")
-	case 0xF7:
-		fmt.Println("EC")
-	case 0xF8:
-		fmt.Println("EL")
-	case 0xF9:
-		fmt.Println("GA")
-	case 0xFA:
-		fmt.Println("SB")
-	case 0xFB:
-		fmt.Println("WILL")
-	case 0xFC:
-		fmt.Println("WONT")
-	case 0xFD:
-		fmt.Println("DO")
-	case 0xFE:
-		fmt.Println("DONT")
-	case 0xFF:
-		fmt.Println("IAC")
-	default:
-		r = true
-	}
-	return
-}
-
-func sendSetup(conn *net.TCPConn) (err error) {
-	conn.Write([]byte{255, 251, 3})
-	conn.Write([]byte{255, 251, 1})
-
-	var buf []byte
-	buf, err = read(conn, 6)
-	if err != nil {
-		return
-	}
-	if !bytes.Equal(buf[0:6], []byte{255, 253, 3, 255, 253, 1}) {
-		log.Errorln("setup failed ", buf)
-	}
-	return
-}
-
-func read(conn *net.TCPConn, size int) (rbuf []byte, err error) {
-	buf := make([]byte, size+1024)
-	var rLen int
-	var totLen int
-	for {
-		rLen, err = conn.Read(buf)
-		if err != nil {
-			return
-		}
-		totLen += rLen
-		for i := 0; i < rLen; i++ {
-			rbuf = append(rbuf, buf[i])
-		}
-		if totLen >= size {
-			break
-		}
-	}
-	return
-}
-
 func handleRequest(conn *net.TCPConn) {
-
-	sendSetup(conn)
 	cChar := make(chan byte)
 	cErr := make(chan error)
+
+	telnet.SendSetup(conn)
 
 	defer conn.Close()
 
