6230d56073dd459978e1868008cb668c8795f0a6
Cesar Gimenes <crg@crg.eti.br>
2017-07-02 09:24:40 -0300

    add signal control

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+
 	"github.com/crgimenes/atomic/config"
 	"github.com/crgimenes/atomic/server"
 	log "github.com/nuveo/logSys"
@@ -12,6 +16,20 @@ func main() {
 	if err != nil {
 		log.Errorln(err)
 	}
+
+	go func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, os.Interrupt)
+		<-sc
+
+		log.Warningln("\r\nshutting down...\r\n")
+
+		// clear
+
+		fmt.Printf("have a nice day!\r\n")
+		os.Exit(0)
+	}()
+
 	server.Run()
 
 }
