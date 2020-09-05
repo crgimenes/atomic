dc220bf0fffe3ba16188df8c3a1b6cb6e63327cb
Cesar Gimenes <crg@crg.eti.br>
2020-09-05 12:25:49 -0300

    test ascii art

--- fixtures/init.lua
+++ fixtures/init.lua
@@ -1,5 +1,15 @@
+cls()
 print("print in server terminal")
 write("\r\nthis is a test write to client instance\r\n")
+write(" ██████╗██████╗  ██████╗    ███████╗████████╗██╗   ██████╗ ██████╗\r\n")
+write("██╔════╝██╔══██╗██╔════╝    ██╔════╝╚══██╔══╝██║   ██╔══██╗██╔══██╗\r\n")
+write("██║     ██████╔╝██║  ███╗   █████╗     ██║   ██║   ██████╔╝██████╔╝\r\n")
+write("██║     ██╔══██╗██║   ██║   ██╔══╝     ██║   ██║   ██╔══██╗██╔══██╗\r\n")
+write("╚██████╗██║  ██║╚██████╔╝██╗███████╗   ██║   ██║██╗██████╔╝██║  ██║\r\n")
+write(" ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝╚══════╝   ╚═╝   ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝\r\n")
+write("crg@crg.eti.br @crgimenes\r\n")
+write("██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██\r\n")
+
 
 function exitConnection()
     print("quit user")
@@ -19,7 +29,6 @@ function toggleEcho()
 end
 
 setANSI(1,4,31)
-cls()
 write("[e] toggle echo on/off\r\n")
 write("[a] print test string\r\n")
 write("[q] quit\r\n")
