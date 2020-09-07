697293d642afa6a8c215bc1c169bef696bc2a0bc
Cesar Gimenes <crg@crg.eti.br>
2020-09-07 12:06:46 -0300

    add client test

---
+++ client/client_test.go
@@ -0,0 +1,38 @@
+package client
+
+import (
+	"reflect"
+	"testing"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func TestNewInstance(t *testing.T) {
+	type args struct {
+		conn ssh.Channel
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Instance
+	}{
+		{
+			name: "success",
+			args: args{
+				conn: nil,
+			},
+			want: &Instance{
+				Conn: nil,
+				H:    25,
+				W:    80,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewInstance(tt.args.conn); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewInstance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
