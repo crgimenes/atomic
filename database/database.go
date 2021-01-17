bf0bf869c0fb6c3f6ea6a03a7dc4bae68d9fd46b
Cesar Gimenes <crg@crg.eti.br>
2021-01-16 22:16:59 -0300

    mod database interface

--- database/database.go
+++ database/database.go
@@ -3,4 +3,17 @@ package database
 type Database interface {
 	Open(name string) error
 	Close() error
+	List() ([]Bucket, error)
+}
+
+type Bucket interface {
+	Name() string
+	List() ([]string, error)
+	Save(ID string, value Data) error
+	Load(ID string, value *Data) error
+	String() string
+}
+
+type Data interface {
+	String() string
 }
