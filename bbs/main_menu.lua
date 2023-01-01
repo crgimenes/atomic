d9ac8190be8f38b64793852c26a4824cada9f06e
crgimenes <crg@crg.eti.br>
2023-01-01 08:14:19 -0300

    update path and test menus

--- bbs/main_menu.lua
+++ bbs/main_menu.lua
@@ -10,7 +10,7 @@ function Main_menu()
     clearTriggers()
     Term.cls()
     Term.write("\r\nmain menu\r\n")
-    Term.write("[1] love coding\r\n")
+    Term.write("[1] live coding\r\n")
     Term.write("[0] back\r\n")
 
 
