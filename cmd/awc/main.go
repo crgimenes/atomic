aa08c05fa178a2c18a87a1d1f25cd043706dd376
crgimenes <crg@crg.eti.br>
2023-01-18 23:49:56 -0300

    add websocket

--- cmd/awc/main.go
+++ cmd/awc/main.go
@@ -2,11 +2,66 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
 )
 
-//go:embed static
-var static embed.FS
+//go:embed assets
+var assets embed.FS
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	index, err := assets.ReadFile("assets/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, err := template.New("index.html").Parse(string(index))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// retorna o template renderizado
+	err = t.Execute(w, nil)
+
+}
+
+func echoServer(ws *websocket.Conn) {
+	defer ws.Close()
+	for {
+		var message string
+		if err := websocket.Message.Receive(ws, &message); err != nil {
+			fmt.Println("Error reading from websocket:", err)
+			break
+		}
+		fmt.Println("Received message:", message)
+		if err := websocket.Message.Send(ws, message); err != nil {
+			fmt.Println("Error sending to websocket:", err)
+			break
+		}
+	}
+}
 
 func main() {
-	// ...
+	port := 8080
+	mux := http.NewServeMux()
+
+	mux.Handle("/assets/", http.FileServer(http.FS(assets)))
+	mux.Handle("/echo", websocket.Handler(echoServer))
+	mux.HandleFunc("/", homeHandler)
+
+	s := &http.Server{
+		Handler:        mux,
+		Addr:           fmt.Sprintf(":%d", port),
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   5 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Printf("Listening on port %d\n", port)
+	log.Fatal(s.ListenAndServe())
 }
--- go.mod
+++ go.mod
@@ -7,22 +7,23 @@ require (
 	github.com/creack/pty v1.1.18
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/kr/pty v1.1.8
-	github.com/yuin/gopher-lua v0.0.0-20221210110428-332342483e3f
-	golang.org/x/crypto v0.4.0
-	golang.org/x/term v0.3.0
-	modernc.org/sqlite v1.20.1
+	github.com/yuin/gopher-lua v1.0.0
+	golang.org/x/crypto v0.5.0
+	golang.org/x/net v0.5.0
+	golang.org/x/term v0.4.0
+	modernc.org/sqlite v1.20.2
 )
 
 require (
-	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20220927061507-ef77025ab5aa // indirect
 	golang.org/x/mod v0.7.0 // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/tools v0.4.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/tools v0.5.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/uint128 v1.2.0 // indirect
