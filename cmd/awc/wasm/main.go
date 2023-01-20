46160869c82ad0e23feeddbb3c737364d9e9c749
crgimenes <crg@crg.eti.br>
2023-01-20 05:11:29 -0300

    capture keyboard

--- cmd/awc/wasm/main.go
+++ cmd/awc/wasm/main.go
@@ -21,6 +21,35 @@ func main() {
 	// call JS function from Go
 	js.Global().Call("sendToServer", "JS sendToServer called from GO")
 
+	// pega o canvas no html
+	canvas := js.Global().Get("document").Call("getElementById", "canvas")
+	// pega o contexto 2d do canvas
+	ctx := canvas.Call("getContext", "2d")
+
+	// ajusta o tamanho da imagem dentro do canvas para 800x600
+	//canvas.Set("width", 800)
+	//canvas.Set("height", 600)
+
+	// desenha um retangulo vermelho
+	ctx.Set("fillStyle", "red")
+	ctx.Call("fillRect", 10, 10, 100, 100)
+
+	// desenha pixel branco na posicao 10, 10 do canvas
+	ctx.Set("fillStyle", "white")
+	ctx.Call("fillRect", 10, 10, 1, 1)
+
 	c := make(chan struct{}, 0)
 	<-c
 }
+
+/*
+
+ws := js.Global().Get("WebSocket").New("ws://localhost:8080/ws")
+
+ws.Call("addEventListener", "open", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+    fmt.Println("open")
+
+    ws.Call("send", js.TypedArrayOf([]byte{123}))
+    return nil
+}))
+*/
