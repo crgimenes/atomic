91467a4fe56312cc381d47eca9fe38e9b9259c18
crgimenes <crg@crg.eti.br>
2023-10-10 22:55:21 -0300

    add mouse test

--- cmd/torminal/main.go
+++ cmd/torminal/main.go
@@ -14,6 +14,12 @@ var (
 	height int
 )
 
+// get mouse position.
+// echo "\e[?1003h\e[?1015h\e[?1006h";
+
+// stop getting mouse position.
+// echo -e "\e[?1000;1006;1015l"
+
 func main() {
 
 	var err error
@@ -51,6 +57,9 @@ func main() {
 	// clear screen.
 	fmt.Printf("\033[2J")
 
+	// start get mouse position.
+	fmt.Printf("\033[?1003h\033[?1015h\033[?1006h")
+
 	// handle input.
 	for {
 		var r rune
@@ -66,17 +75,21 @@ func main() {
 			// clear screen.
 			fmt.Printf("\033[2J")
 
-			os.Exit(0)
+			// stop get mouse position.
+			fmt.Printf("\033[?1000;1006;1015l")
+
+			return
+
 		default:
 			// save cursor position.
 			//fmt.Printf("\033[s")
-			//fmt.Printf("Read: %q 0x%X width: %d height: %d\r\n", r, r, width, height)
+			fmt.Printf("Read: %q 0x%X width: %d height: %d\r\n", r, r, width, height)
 
 			// restore cursor position.
 			//fmt.Printf("\033[u")
 		}
 
-		fmt.Printf("%c", r)
+		//fmt.Printf("%c", r)
 
 	}
 
